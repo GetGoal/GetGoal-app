69e26f1a589086dfad23e6fbd9550b0754bba6ba
xbklyn <nut.chotiwit@gmail.com>
2023-11-15 02:29:57 +0700

    [MODIFIED] remove on update as already declared BeforeUpdate()

--- modules/label/model.go
+++ modules/label/model.go
@@ -11,7 +11,7 @@ type Label struct {
 	LabelID   uint64     `gorm:"column:label_id;primary_key;auto_increment" json:"label_id"`
 	LabelName string     `gorm:"column:label_name;type:varchar(50);not null" json:"label_name"`
 	CreatedAt time.Time  `gorm:"column:created_at;not null;default:current_timestamp" json:"created_at"`
-	UpdatedAt time.Time  `gorm:"column:updated_at;not null;default:current_timestamp on update current_timestamp" json:"updated_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at;not null;default:current_timestamp" json:"updated_at"`
 	DeletedAt *time.Time `gorm:"column:deleted_at;index" json:"deleted_at"`
 }
 
