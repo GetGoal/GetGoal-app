9b8eb111727c09d07f8fed9aa08ed49647a4b792
xbklyn <nut.chotiwit@gmail.com>
2023-11-15 02:15:10 +0700

    [ADD] global file for routers

---
+++ routes/routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xbklyn/getgoal-app/modules/label"
+)
+
+func GetRoutes() *gin.Engine {
+	r := gin.Default()
+
+	v1 := r.Group("/api/v1")
+	label.LabelAnonymousRegister(v1.Group("/labels"))
+
+	landing := r.Group("/api/pong")
+	landing.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r
+}
