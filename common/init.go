736005cd098a605735c442a6f106720f29c17283
xbklyn <nut.chotiwit@gmail.com>
2023-11-16 00:48:21 +0700

    [MODIFIED] load env logics

--- common/init.go
+++ common/init.go
@@ -2,13 +2,23 @@ package common
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
+	// Check if .env.local file exists
+	envFile := ".env.local"
+	if _, err := os.Stat(envFile); os.IsNotExist(err) {
+		log.Default().Println("No .env.local file found, using .env.prod")
+		// If .env.local doesn't exist, use .env.prod
+		envFile = ".env.prod"
+	}
+
+	// Load environment variables from the chosen .env file
+	err := godotenv.Load(envFile)
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading %s file", envFile)
 	}
 }
