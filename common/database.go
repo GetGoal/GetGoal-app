92af8ffa42a0ca36bf7159aecaa261db97d910f7
xbklyn <nut.chotiwit@gmail.com>
2023-11-19 01:19:18 +0700

    [MODIFIED] fix typo

--- common/database.go
+++ common/database.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func GetDbUrl() string {
-	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_"))
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	// ="host=${DB_HOST} user=${DB_USER} password=${DB_PASSWORD} dbname=${DB_NAME} port=${DB_PORT} sslmode=disable"
 }
