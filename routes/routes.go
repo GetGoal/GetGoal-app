2e534505a46ee5f9d6ec4a976a2e018a443c62f8
xbklyn <nut.chotiwit@gmail.com>
2023-11-18 18:21:36 +0700

    [MODIFIED] edit route path

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/xbklyn/getgoal-app/modules/label"
 )
@@ -8,13 +10,20 @@ import (
 func GetRoutes() *gin.Engine {
 	r := gin.Default()
 
-	r.RedirectTrailingSlash = false
+	// r.RedirectTrailingSlash = false
 
-	v1 := r.Group("/api/v1")
+	api := r.Group("/api/" + func() string {
+		if os.Getenv("ENV") == "qa" {
+			return "qa"
+		}
+		return ""
+	}())
+
+	v1 := api.Group("/v1")
 	label.LabelAnonymousRegister(v1.Group("/labels"))
 	label.LabelRegister(v1.Group("/labels"))
 
-	landing := r.Group("/api/ping")
+	landing := api.Group("/ping")
 	landing.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
