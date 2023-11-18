430a38e847032689b85e5376287cdf00e5528586
xbklyn <nut.chotiwit@gmail.com>
2023-11-18 09:38:12 +0700

    [MODIFIED] remove parameter

--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,6 @@ func GetRoutes() *gin.Engine {
 	r := gin.Default()
 
 	r.RedirectTrailingSlash = false
-	r.RemoveExtraSlash = true
 
 	v1 := r.Group("/api/v1")
 	label.LabelAnonymousRegister(v1.Group("/labels"))
