3fcdf0998a22b27543546557210dcfef7da1c4e0
xbklyn <nut.chotiwit@gmail.com>
2023-11-19 17:09:40 +0700

    [MODIFIED] When get all program,it will return with data with labels

--- modules/program/model.go
+++ modules/program/model.go
@@ -1,6 +1,8 @@
 package program
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/xbklyn/getgoal-app/common"
@@ -17,11 +19,32 @@ type Program struct {
 	CreatedAt          time.Time  `gorm:"column:created_at;not null;default:current_timestamp" json:"created_at"`
 	UpdatedAt          time.Time  `gorm:"column:updated_at;not null;default:current_timestamp" json:"updated_at"`
 	DeletedAt          *time.Time `gorm:"column:deleted_at;index" json:"deleted_at"`
+
+	// Relationships
+	Labels []Label `gorm:"many2many:label_program;foreignKey:ProgramID;joinForeignKey:ProgramID;References:LabelID;JoinReferences:LabelID" json:"labels"`
 }
 
-func (Program) TableName() string {
+type Label struct {
+	LabelID   uint64 `gorm:"column:label_id;primary_key;auto_increment" json:"label_id"`
+	LabelName string `gorm:"column:label_name;type:varchar(50);not null" json:"label_name"`
+}
+
+func Migrate() {
+	db := common.GetDB()
+	db.AutoMigrate(&Program{}, &Label{})
+}
+
+func (program *Program) TableName() string {
 	return "program"
 }
+
+func (label *Label) TableName() string {
+	return "label"
+}
+
+// func (labelProgram *LabelProgram) TableName() string {
+// 	return "label_program"
+// }
 
 func (program *Program) BeforeUpdate(tx *gorm.DB) (err error) {
 	now := time.Now()
@@ -32,9 +55,21 @@ func (program *Program) BeforeUpdate(tx *gorm.DB) (err error) {
 func FindAllProgram() ([]Program, error) {
 	db := common.GetDB()
 
+	var program Program
+	if result := db.Preload("Labels").First(&program); result.Error != nil {
+		log.Fatalf("Error during Preload: %v", result.Error)
+	} else {
+		fmt.Printf("Preload Result: %+v\n", program)
+	}
+
 	var programs []Program
 
-	err := db.Find(&programs).Error
+	err := db.Debug().Model(&Program{}).Preload("Labels").Find(&programs).Error
+
+	for _, program := range programs {
+		fmt.Printf("Program ID: %d\n", program.ProgramID)
+		fmt.Printf("Number of Labels: %d\n", len(program.Labels))
+	}
 	return programs, err
 }
 func FindOneProgram(condition interface{}) (Program, error) {
