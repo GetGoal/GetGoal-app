bb961fce564cfe31627128b9c80c917f713d2ed1
xbklyn <nut.chotiwit@gmail.com>
2023-12-08 18:19:50 +0700

    [ADD] Move entity in user module to new filw

--- modules/user_account/model.go
+++ modules/user_account/model.go
@@ -20,22 +20,6 @@ type UserAccount struct {
 	Tasks *[]Task `gorm:"foreignKey:UserAccountID" json:"tasks"`
 }
 
-type Task struct {
-	TaskID            uint64     `gorm:"column:task_id;primary_key;auto_increment" json:"task_id"`
-	TaskName          string     `gorm:"column:task_name;type:varchar(150);not null" json:"task_name"`
-	TaskStatus        int        `gorm:"column:task_status;not null" json:"task_status"`
-	IsSetNotification int        `gorm:"column:is_set_noti;not null" json:"is_set_noti"`
-	StartTime         time.Time  `gorm:"column:start_time;not null" json:"start_time"`
-	EndTime           *time.Time `gorm:"column:end_time" json:"end_time"`
-	UserAccountID     int        `gorm:"column:user_account_id;not null" json:"user_account_id"`
-	Category          string     `gorm:"column:category;type:varchar(50)" json:"category"`
-	TimeBeforeNotify  int        `gorm:"column:time_before_notify" json:"time_before_notify"`
-	TaskDescription   string     `gorm:"column:task_description;type:varchar(250)" json:"task_description"`
-	Link              string     `gorm:"column:link;type:varchar(255)" json:"link"`
-	MediaURL          string     `gorm:"column:media_url;type:varchar(255)" json:"media_url"`
-	UpdatedAt         time.Time  `gorm:"column:updated_at;not null;default:current_timestamp" json:"updated_at"`
-}
-
 func Migrate() {
 	db := common.GetDB()
 	db.AutoMigrate(&UserAccount{}, &Task{})
@@ -43,10 +27,6 @@ func Migrate() {
 
 func (user *UserAccount) TableName() string {
 	return "user_account"
-}
-
-func (task *Task) TableName() string {
-	return "task"
 }
 
 func (user *UserAccount) BeforeUpdate(tx *gorm.DB) (err error) {
