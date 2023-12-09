d4046aada6cc1cc98a59222d036fed15d0421493
xbklyn <nut.chotiwit@gmail.com>
2023-12-09 19:55:55 +0700

    [MODIFIED] edit bindind method

--- modules/task/validator.go
+++ modules/task/validator.go
@@ -9,8 +9,10 @@ import (
 )
 
 type GetTaskByEmailAndDateValidator struct {
-	Email string `json:"email" binding:"required"`
-	Date  string `json:"date" binding:"required"`
+	GetTaskByEmailAndDateTask struct {
+		Email string `json:"email" binding:"required"`
+		Date  string `json:"date" binding:"required"`
+	} `json:"params" binding:"required"`
 
 	getTaskByEmailAndDateModel GetTaskByEmailAndDateTask
 }
@@ -20,20 +22,19 @@ func NewGetTaskByEmailandDateValidator() GetTaskByEmailAndDateValidator {
 }
 
 func (s *GetTaskByEmailAndDateValidator) Bind(c *gin.Context) error {
-
-	err := c.BindJSON(s)
+	err := c.ShouldBindJSON(s)
 	if err != nil {
 		log.Default().Printf("Error binding JSON: %s\n", err.Error())
 		return err
 	}
 
-	parsedDate, err := time.Parse(time.RFC3339, s.Date)
+	parsedDate, err := time.Parse(time.RFC3339, s.GetTaskByEmailAndDateTask.Date)
 	if err != nil {
 		fmt.Printf("Error parsing date: %s\n", err.Error())
 		return err
 	}
 
-	s.getTaskByEmailAndDateModel.Email = s.Email
+	s.getTaskByEmailAndDateModel.Email = s.GetTaskByEmailAndDateTask.Email
 	s.getTaskByEmailAndDateModel.Date = parsedDate
 
 	fmt.Printf("Binded JSON payload: %+v\n", s.getTaskByEmailAndDateModel)
