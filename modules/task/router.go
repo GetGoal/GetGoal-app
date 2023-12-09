9c78db804afdbc381d4c4b6f5d123d1cc9f436ad
xbklyn <nut.chotiwit@gmail.com>
2023-12-10 04:49:43 +0700

    [MODIFIED] when plan will convert date to today's date

--- modules/task/router.go
+++ modules/task/router.go
@@ -1,8 +1,10 @@
 package task
 
 import (
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xbklyn/getgoal-app/common"
@@ -10,12 +12,12 @@ import (
 )
 
 func TaskRegister(router *gin.RouterGroup) {
-	// router.POST("", ProgramCreate)
 	router.GET("/to-do", TaskFromEmailAndDate)
 }
 func TaskAnonymousRegister(router *gin.RouterGroup) {
 	router.GET("", TaskList)
 	router.GET("/:id", TaskDetail)
+	router.GET("/plan/:program_id", TaskPlanning)
 }
 
 func TaskList(c *gin.Context) {
@@ -66,6 +68,48 @@ func TaskFromEmailAndDate(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusNotFound, common.NewError("Task", err))
 		return
+	}
+
+	serializer := TasksSerializer{C: c, Tasks: tasks, Count: len(tasks)}
+	c.JSON(http.StatusOK, gin.H{"Task": serializer.Response()})
+}
+
+func TaskPlanning(c *gin.Context) {
+	programId, err := strconv.ParseUint(c.Param("program_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, common.NewError("Program", err))
+		return
+	}
+
+	tasks, err := GetTaskByProgramId(programId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, common.NewError("Task", err))
+		return
+	}
+
+	now := time.Now()
+	var originalStartTime time.Time
+
+	for i := range tasks {
+		log.Println("=====", i, "=====")
+		originalTime := tasks[i].StartTime.Format("15:04:05")
+
+		if i > 0 {
+
+			diff := tasks[i].StartTime.Sub(originalStartTime)
+			newStartTime := tasks[i-1].StartTime.Add(diff)
+
+			originalStartTime = tasks[i].StartTime
+			tasks[i].StartTime = newStartTime
+			continue
+		} else {
+
+			newStartTime, _ := time.Parse(time.RFC3339, now.Format("2006-01-02")+"T"+originalTime+"Z")
+
+			originalStartTime = tasks[i].StartTime
+			tasks[i].StartTime = newStartTime
+			continue
+		}
 	}
 
 	serializer := TasksSerializer{C: c, Tasks: tasks, Count: len(tasks)}
