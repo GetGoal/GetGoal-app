0b8c50023684485f1c4e471b36eb025076cf9aec
xbklyn <nut.chotiwit@gmail.com>
2023-12-10 05:11:23 +0700

    [MODIFIED] fix

--- common/init.go
+++ common/init.go
@@ -3,6 +3,7 @@ package common
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -23,6 +24,17 @@ func LoadEnvVariables() {
 	log.Default().Println("Currenly using " + envFile + " environment")
 }
 
-func SetTimeZone() {
-	os.Setenv("TZ", "Asia/Bangkok")
+var loc *time.Location
+
+func SetTimezone(tz string) error {
+	location, err := time.LoadLocation(tz)
+	if err != nil {
+		return err
+	}
+	loc = location
+	return nil
+}
+
+func GetTime(t time.Time) time.Time {
+	return t.In(loc)
 }
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/xbklyn/getgoal-app/SQL/migration"
 	"github.com/xbklyn/getgoal-app/common"
 	"github.com/xbklyn/getgoal-app/routes"
 )
 
 func init() {
+	common.SetTimezone("Asia/Bangkok")
+	t := common.GetTime(time.Now())
+	log.Println(t)
 	common.LoadEnvVariables()
-	common.SetTimeZone()
 	common.InitDB()
 }
 func main() {
