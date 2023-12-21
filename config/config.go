8e8cdffbef39d0e4c396d33e13039e22e18b9ba4
xbklyn <nut.chotiwit@gmail.com>
2023-12-21 17:29:43 +0700

    [MODIFIED] set to env

--- config/config.go
+++ config/config.go
@@ -47,7 +47,7 @@ type (
 func GetConfig() Config {
 	// Set up Viper
 	viper.AutomaticEnv() // Automatically read from environment variables
-	viper.SetConfigType("yaml")
+	viper.SetConfigType("env")
 
 	log.Default().Println("Binding environment variables...")
 	viper.BindEnv("env")
