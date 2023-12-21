b9c9693f53329e280bf644c9cc0ec4f95b442232
xbklyn <nut.chotiwit@gmail.com>
2023-12-21 18:29:27 +0700

    proof

--- config/config.go
+++ config/config.go
@@ -49,7 +49,16 @@ func GetConfig() Config {
 	viper.AutomaticEnv() // Automatically read from environment variables
 
 	log.Default().Println("Binding environment variables...")
-
+	// Bind environment variables
+	viper.BindEnv("ENV")
+	viper.BindEnv("DATABASE_HOST")
+	viper.BindEnv("DATABASE_PORT")
+	viper.BindEnv("DATABASE_USER")
+	viper.BindEnv("DATABASE_PASSWORD")
+	viper.BindEnv("DATABASE_DBNAME")
+	viper.BindEnv("DATABASE_SSLMODE")
+	viper.BindEnv("DATABASE_TIMEZONE")
+	//Set default values
 	viper.Set("env", viper.BindEnv("ENV"))
 	viper.Set("database.host", viper.BindEnv("DATABASE_HOST"))
 	viper.Set("database.port", viper.BindEnv("DATABASE_PORT"))
@@ -58,8 +67,18 @@ func GetConfig() Config {
 	viper.Set("database.dbname", viper.BindEnv("DATABASE_DBNAME"))
 	viper.Set("database.sslmode", viper.BindEnv("DATABASE_SSLMODE"))
 	viper.Set("database.timezone", viper.BindEnv("DATABASE_TIMEZONE"))
-
-	log.Default().Printf("ENV in docker container : ")
+	//Log values
+	log.Default().Println("ENV in docker container : ")
+	log.Default().Printf("Env %s", viper.GetString("env"))
+	log.Default().Printf("DbHost %s", viper.GetString("database.host"))
+	log.Default().Printf("DbPort %d", viper.GetInt("database.port"))
+	log.Default().Printf("DbUser %s", viper.GetString("database.user"))
+	log.Default().Printf("DbPass %s", viper.GetString("database.password"))
+	log.Default().Printf("DbName %s", viper.GetString("database.dbname"))
+	log.Default().Printf("SSLMode %s", viper.GetString("database.sslmode"))
+	log.Default().Printf("TimeZone %s", viper.GetString("database.timezone"))
+	//Log values
+	log.Default().Printf("ENV after set : ")
 	log.Default().Printf("Env %s", viper.GetString("env"))
 	log.Default().Printf("DbHost %s", viper.GetString("database.host"))
 	log.Default().Printf("DbPort %d", viper.GetInt("database.port"))
