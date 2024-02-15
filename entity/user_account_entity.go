8fad1064e67c45635eb7c260f72fc070ea45784a
xbklyn <nut.chotiwit@gmail.com>
2024-02-15 23:20:27 +0700

    Update user entity

--- entity/user_account_entity.go
+++ entity/user_account_entity.go
@@ -7,13 +7,20 @@ import (
 )
 
 type UserAccount struct {
-	UserID    uint64     `gorm:"column:user_id;primary_key;auto_increment" json:"user_id"`
-	FirstName string     `gorm:"column:first_name;type:varchar(70);not null" json:"first_name"`
-	LastName  string     `gorm:"column:last_name;type:varchar(70);not null" json:"last_name"`
-	Email     string     `gorm:"column:email;type:varchar(100);not null" json:"email"`
-	CreatedAt time.Time  `gorm:"column:created_at;not null;default:current_timestamp" json:"created_at"`
-	UpdatedAt time.Time  `gorm:"column:updated_at;not null;default:current_timestamp" json:"updated_at"`
-	DeletedAt *time.Time `gorm:"column:deleted_at" json:"deleted_at"`
+	UserID                uint64     `gorm:"column:user_id;primary_key;auto_increment" json:"user_id"`
+	FirstName             string     `gorm:"column:first_name;type:varchar(70);not null" json:"first_name"`
+	LastName              string     `gorm:"column:last_name;type:varchar(70);not null" json:"last_name"`
+	Email                 string     `gorm:"column:email;type:varchar(100);not null" json:"email"`
+	EmailValidationID     int        `gorm:"column:email_validation_id;type:varchar(100);not null" json:"email_validation_id"`
+	PasswordHash          string     `gorm:"column:password_hash;type:varchar(250);not null" json:"password_hash"`
+	PasswordSalt          string     `gorm:"column:password_salt;type:varchar(250);not null" json:"password_salt"`
+	ConfirmationToken     string     `gorm:"column:confirmation_token;type:varchar(250);not null" json:"confirmation_token"`
+	TokenGenerationTime   time.Time  `gorm:"column:token_generation_time;not null;default:current_timestamp" json:"token_generation_time"`
+	PasswordRecoveryToken string     `gorm:"column:password_recovery_token;type:varchar(100);not null" json:"password_recovery_token"`
+	RecoveryTokenTime     time.Time  `gorm:"column:recovery_token_time;not null;default:current_timestamp" json:"recovery_token_time"`
+	CreatedAt             time.Time  `gorm:"column:created_at;not null;default:current_timestamp" json:"created_at"`
+	UpdatedAt             time.Time  `gorm:"column:updated_at;not null;default:current_timestamp" json:"updated_at"`
+	DeletedAt             *time.Time `gorm:"column:deleted_at" json:"deleted_at"`
 
 	//Relationship
 	Tasks      *[]Task      `gorm:"foreignKey:UserAccountID" json:"tasks"`
