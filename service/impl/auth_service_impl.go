0aed52907f65de14e401f686f62a6e0cd80603be
xbklyn <nut.chotiwit@gmail.com>
2024-03-24 10:21:23 +0700

    move creating hash password to common package

--- service/impl/auth_service_impl.go
+++ service/impl/auth_service_impl.go
@@ -1,13 +1,10 @@
 package impl
 
 import (
-	"crypto/subtle"
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
 	"math/rand"
-	"strings"
 	"time"
 
 	"github.com/xbklyn/getgoal-app/common"
@@ -16,24 +13,7 @@ import (
 	"github.com/xbklyn/getgoal-app/model"
 	repository "github.com/xbklyn/getgoal-app/repository"
 	"github.com/xbklyn/getgoal-app/service"
-	"golang.org/x/crypto/argon2"
 )
-
-type params struct {
-	memory      uint32
-	iterations  uint32
-	parallelism uint8
-	saltLength  uint32
-	keyLength   uint32
-}
-
-var p = &params{
-	memory:      64 * 1024, // 64 MB
-	iterations:  3,
-	parallelism: 1,
-	saltLength:  16,
-	keyLength:   32,
-}
 
 func NewAuthServiceImpl(userRepo repository.UserRepo, mailer service.MailerService) service.AuthService {
 	return &AuthServiceImpl{userRepo, mailer, make(map[string]bool), common.Claims{}}
@@ -123,7 +103,7 @@ func (service *AuthServiceImpl) SignIn(request model.Credentials) (string, strin
 		return "", "", errors.New("user is not verified")
 	}
 	//check if password is correct
-	match, err := verifyPassword(request.Password, user.PasswordSalt)
+	match, err := common.VerifyPassword(request.Password, user.PasswordSalt)
 	if err != nil {
 		return "", "", err
 	}
@@ -179,7 +159,7 @@ func (service *AuthServiceImpl) SignUp(request model.SignUpRequest) (useEntityr 
 		return entity.UserAccount{}, errors.New("this email is already registered")
 	}
 	//gen passwod
-	hashed, encodedHash, err := generateHashFromPassword(request.Password, p)
+	hashed, encodedHash, err := common.GenerateHashFromPassword(request.Password)
 	if err != nil {
 		return entity.UserAccount{}, err
 	}
@@ -267,55 +247,6 @@ func (service *AuthServiceImpl) signInWithGoogle(request model.GoogleSignInReque
 	return access, refresh, nil
 }
 
-func generateHashFromPassword(password string, p *params) (hashed string, encodedHash string, err error) {
-	salt, err := generateRandomBytes(p.saltLength)
-	if err != nil {
-		return "", "", err
-	}
-
-	hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
-
-	fmt.Print(string(hash))
-	// Base64 encode the salt and hashed password.
-	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
-	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
-
-	// Return a string using the standard encoded hash representation.
-	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
-
-	return b64Hash, encodedHash, nil
-}
-
-func generateRandomBytes(n uint32) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
-}
-
-func verifyPassword(password, encodedHash string) (match bool, err error) {
-	// Extract the parameters, salt and derived key from the encoded password
-	// hash.
-	p, salt, hash, err := decodeHash(encodedHash)
-	if err != nil {
-		return false, err
-	}
-
-	// Derive the key from the other password using the same parameters.
-	otherHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
-
-	// Check that the contents of the hashed passwords are identical. Note
-	// that we are using the subtle.ConstantTimeCompare() function for this
-	// to help prevent timing attacks.
-	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
-		return true, nil
-	}
-	return false, nil
-}
-
 func generateVerificationCode(length int) string {
 	// Define the character set for the verification code (numbers only)
 	charSet := "0123456789"
@@ -329,39 +260,4 @@ func generateVerificationCode(length int) string {
 	}
 
 	return string(verificationCode)
-}
-func decodeHash(encodedHash string) (p *params, salt, hash []byte, err error) {
-	vals := strings.Split(encodedHash, "$")
-	if len(vals) != 6 {
-		return nil, nil, nil, errors.New("the encoded hash is not in the correct format")
-	}
-
-	var version int
-	_, err = fmt.Sscanf(vals[2], "v=%d", &version)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	if version != argon2.Version {
-		return nil, nil, nil, errors.New("incompatible version of argon2")
-	}
-
-	p = &params{}
-	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-
-	salt, err = base64.RawStdEncoding.Strict().DecodeString(vals[4])
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	p.saltLength = uint32(len(salt))
-
-	hash, err = base64.RawStdEncoding.Strict().DecodeString(vals[5])
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	p.keyLength = uint32(len(hash))
-
-	return p, salt, hash, nil
 }
