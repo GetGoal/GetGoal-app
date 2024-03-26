4427d8fcd390c2975a86a46d9a3cf736a3d75692
xbklyn <nut.chotiwit@gmail.com>
2024-03-27 00:32:28 +0700

    edit typo

--- service/user_service.go
+++ service/user_service.go
@@ -8,5 +8,5 @@ import (
 
 type UserService interface {
 	FindUserByEmail(c *gin.Context) (*entity.UserAccount, error)
-	ResetPassword(c *gin.Context, credentail model.ChangePasswordRequest) (*entity.UserAccount, error)
+	ResetPassword(c *gin.Context, credential model.ChangePasswordRequest) (*entity.UserAccount, error)
 }
